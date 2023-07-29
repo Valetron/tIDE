098f88af39032a14d9875c02040b864745016127
Valetron <notspammeplz@gmail.com>
2023-07-30 02:37:44 +0300

    working \n, thanks chatGPT

--- main.c
+++ main.c
@@ -1,32 +1,88 @@
-#include <string.h>
+//#include <string.h>
+
+//#include <ncurses.h>
+
+//#include "src/init.h"
+
+//enum COLORS
+//{
+//    COLOR_PINK = 10
+//};
+
+//int main(int argc, char** argv)
+//{
+//	//raw();                 //Берём управление клавиатурой на себя
+//	//cbreak();				// отменяет действие raw()
+//    int ch;
+
+//	//init();
+
+//	initscr();
+//	cbreak();
+//	noecho();
+
+//	int currX = getmaxx(stdscr);
+//	int currY = getmaxy(stdscr);
+
+//	if (currX < MINIMUM_X || currY < MINIMUM_Y)
+//	{
+//		puts("Terminal size is too small");
+//		//exit_curses(1);
+//		goto EXIT;
+//	}
+
+//	while ( (ch = getch()) != KEY_F(1))
+//	{
+//		switch (ch)
+//		{
+//		case KEY_ENTER:
+//			//mvprintw(2, 2, "Pressed");
+//			printw("Works");
+//			refresh();
+//			break;
+		
+//		default:
+//			break;
+//		}
+//		//if (ARR)
+//		if (KEY_ENTER == ch)
+//		{
+//			mvprintw(2, 2, "Pressed");
+//			//move(getcury(stdscr) + 1, 0);
+//			refresh();
+//		}
+//	}
+
+//EXIT:
+//	endwin();
+//	return 0;
+//}
+
 
 #include <ncurses.h>
 
-#include "src/init.h"
+int main() {
+    initscr();
 
-enum COLORS
-{
-    COLOR_PINK = 10
-};
+    // disable line buffering and echoing of typed characters
+    //cbreak();
+	//raw();
+    //noecho();
 
-int main(int argc, char** argv)
-{
-	//raw();                 //Берём управление клавиатурой на себя
-	//cbreak();				// отменяет действие raw()
+    // print a message and wait for a key press
+    printw("Press Enter to move to a new line (Press 'q' to quit) ");
+    refresh();
+
     int ch;
+    while ((ch = getch()) != 27) {
+        if (ch == '\n') {
+            // move cursor to a new line
+            move(getcury(stdscr) + 1, 0);
+            refresh();
+        }
+    }
 
-	init();
+    endwin();
 
-	while ( (ch = getch()) != 27)
-	{
-		if (KEY_ENTER == ch)
-		{
-			mvprintw(2, 2, "Pressed");
-			//move(getcury(stdscr) + 1, 0);
-			refresh();
-		}
-	}
-
-	endwin();
-	return 0;
+    return 0;
 }
