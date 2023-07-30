a78ea3257e68d7c310e34a29e532ca2fea4f700d
Valetron <notspammeplz@gmail.com>
2023-07-31 01:45:53 +0300

    Добавлена функция удаления символов и управление стрелочками

--- main.c
+++ main.c
@@ -59,29 +59,106 @@
 //}
 
 
+//#include <ncurses.h>
+
+//int main() {
+//    initscr();
+
+//    // disable line buffering and echoing of typed characters
+//	raw();
+//    //cbreak();
+//    //noecho();
+
+//    // print a message and wait for a key press
+//    printw("Press Enter to move to a new line (Press 'q' to quit) ");
+//    //refresh();
+//    int ch;
+//	char w = 1;
+//    //while ((ch = getch()) != 27)
+//	while (w)
+//	{
+//		cbreak();
+//		ch = getch();
+//        if (ch == '\n') {
+//            // move cursor to a new line
+//            move(getcury(stdscr) + 1, 0);
+//            refresh();
+//        }
+
+//		if (ch == ('s' & 0x1f))
+//			mvprintw(getcury(stdscr) + 1, 0, "Ctrl-S");
+
+//		if (ch == ('q' & 0x1f))
+//			w = 0;
+		
+//		raw();
+//	}
+
+//    endwin();
+
+//    return 0;
+//}
+
+
+
+
 #include <ncurses.h>
 
 int main() {
+    // Initialize ncurses
     initscr();
+    raw();  // Use raw() mode for low-level input
+    noecho();
+	keypad(stdscr, true);
 
-    // disable line buffering and echoing of typed characters
-    //cbreak();
-	//raw();
-    //noecho();
+    // Loop until the user presses Ctrl-C or Ctrl-Z
+    bool running = true;
+    while (running)
+	{
+        // Switch to cbreak() mode temporarily
+        cbreak();
+        int ch = getch();
+        // Switch back to raw() mode
+        raw();
 
-    // print a message and wait for a key press
-    printw("Press Enter to move to a new line (Press 'q' to quit) ");
-    refresh();
-
-    int ch;
-    while ((ch = getch()) != 27) {
-        if (ch == '\n') {
-            // move cursor to a new line
-            move(getcury(stdscr) + 1, 0);
-            refresh();
+        switch (ch)
+		{
+            case ('s' & 0x1f): // Ctrl-S
+                mvprintw(getcury(stdscr) + 1, 0, "Ctrl-S");
+                break;
+            case ('c' & 0x1f): // Ctrl-C
+                running = false;
+                break;
+            case KEY_RESIZE:
+                // Handle window resize event
+                break;
+            case ERR:
+                // Handle input timeout
+                break;
+			case KEY_BACKSPACE:
+			case 127:
+				mvdelch(getcury(stdscr), getcurx(stdscr) - 1);
+				break;
+			case KEY_UP:
+				move(getcury(stdscr) - 1, getcurx(stdscr));
+				break;
+			case KEY_DOWN:
+				move(getcury(stdscr) + 1, getcurx(stdscr));
+				break;
+			case KEY_LEFT:
+				move(getcury(stdscr), getcurx(stdscr) - 1);
+				break;
+			case KEY_RIGHT:
+				move(getcury(stdscr), getcurx(stdscr) + 1);
+				break;
+            default:
+                // Handle other input events
+				printw("%c", ch);
+                break;
         }
     }
 
+    // Clean up ncurses
     endwin();
 
     return 0;
